bb318da9cf5968dbbae14c3f56715643ebbe17fa
Siddharth Akar <siddharth.akar@qoala.id>
2023-09-23 12:30:21 +0530

    Replying to Messages (Using Threads)

--- slack_bot.py
+++ slack_bot.py
@@ -1,5 +1,5 @@
 import slack
-import os,sys
+import os,sys,string
 from dotenv import load_dotenv
 from flask import Flask, request, Response
 from slackeventsapi import SlackEventAdapter
@@ -19,6 +19,8 @@ BOT_ID=client.api_call('auth.test')["user_id"]
 client.chat_postMessage(channel="#test-bot",text="hello welcome to sqlify")
 message_counts={}
 welcome_messages={}
+
+BAD_WORDS=['bad','No','idiot','stupid']
 class WelcomeMessage:
     START_TEXT={
         'type':'section',
@@ -60,6 +62,10 @@ class WelcomeMessage:
         return {'type':'section','text':{'type':'mrkdwn','text':text}}
 
 def send_welcome_message(channel,user):
+    if channel not in welcome_messages:
+        welcome_messages[channel]={}
+    if user in welcome_messages[channel]:
+        return 
     welcome=WelcomeMessage(channel,user)
     message=welcome.get_message()
     response=client.chat_postMessage(**message)
@@ -68,6 +74,10 @@ def send_welcome_message(channel,user):
         welcome_messages[channel]={}
     welcome_messages[channel][user]=welcome
 
+def check_if_bad_words(message):
+    msg=message.lower()
+    msg=msg.translate(str.maketrans('','',string.punctuation))
+    return any(word in msg for word in BAD_WORDS)
 
 @slack_event_adapter.on('message')
 def message(payload):
@@ -83,6 +93,9 @@ def message(payload):
         # client.chat_postMessage(channel=channel_id,text=text)
         if text.lower()=='start':
             send_welcome_message(f'@{user_id}',user_id)
+        elif check_if_bad_words(text):
+            ts=event.get('ts')
+            client.chat_postMessage(channel=channel_id,thread_ts=ts, text="THAT IS A BAD WORD!" )
 
 @slack_event_adapter.on('reaction_added')
 def reaction(payload):
